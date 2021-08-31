321ce7a9279a3b414f2ed328c4f5de4c649647c1
TheDiegoDev <diegoguineasds@gmail.com>
2021-08-31 12:26:16 +0200

    firebase login

--- app/src/main/java/diego/guinea/desarrolloscursos/ui/LoginActivity.kt
+++ app/src/main/java/diego/guinea/desarrolloscursos/ui/LoginActivity.kt
@@ -1,19 +1,90 @@
 package diego.guinea.desarrolloscursos.ui
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import diego.guinea.desarrolloscursos.R
 import kotlinx.android.synthetic.main.loguin_activity.*
 
 class LoginActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.loguin_activity)
-        textRegister.setOnClickListener {
+        setUp()
+    }
+
+
+
+    private fun setUp() {
+        val sp = getSharedPreferences("user", Context.MODE_PRIVATE)
+        if (sp.getString("active", "") == "true"){
+            firebaseAuth(sp.getString("email","").toString(),sp.getString("password","").toString())
+        }
+
+        buttonEntrar.setOnClickListener {
+
+            if (editTextEmail.text.isNotEmpty() &&
+                    editTextTextPassword.text.isNotEmpty()){
+                rememberUser(sp)
+                firebaseAuth(editTextEmail.text.toString(),editTextTextPassword.text.toString())
+            }else{
+                showAlert()
+            }
+        }
+
+        buttonRegistrar.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun firebaseAuth(email: String, password: String) {
+        FirebaseAuth.getInstance()
+            .signInWithEmailAndPassword(
+                email,password
+            ).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+                } else {
+                    showAlert()
+                }
+            }
+    }
+
+    private fun rememberUser(sp: SharedPreferences) {
+        val email = editTextEmail.text.toString()
+        val password = editTextTextPassword.text.toString()
+
+        with(sp.edit()){
+            putString("email", email)
+            putString("password", password)
+            putString("active", "true")
+
+            apply()
+        }
+        showHome(email, ProviderType.BASIC)
+    }
+
+    private fun showAlert(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se a producido un error autenticando al usuario")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+    private fun showHome(email: String, provider: ProviderType){
+        val intent = Intent(this, HomeActivity::class.java).apply {
+            putExtra("email", email)
+            putExtra("provider", provider.name)
+        }
+        startActivity(intent)
+        finish()
     }
 
 }
--- app/src/main/java/diego/guinea/desarrolloscursos/ui/RegisterActivity.kt
+++ app/src/main/java/diego/guinea/desarrolloscursos/ui/RegisterActivity.kt
@@ -1,8 +1,12 @@
 package diego.guinea.desarrolloscursos.ui
 
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import diego.guinea.desarrolloscursos.R
+import kotlinx.android.synthetic.main.register_activity.*
 
 
 class RegisterActivity : AppCompatActivity() {
@@ -10,6 +14,44 @@ class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register_activity)
+        setUp()
+    }
+
+    private fun setUp() {
+        textRegistrate.setOnClickListener {
+            if (editTextTextEmailAddress.text.isNotEmpty() &&
+                    editTextPhone.text.isNotEmpty() &&
+                    editTextTextPassword2.text.isNotEmpty() &&
+                    editTextTextPassword3.text.isNotEmpty() &&
+                    editTextTextPersonName.text.isNotEmpty() &&
+                    editTextTextPassword2.text == editTextTextPassword3.text){
+
+                FirebaseAuth.getInstance()
+                    .createUserWithEmailAndPassword(editTextTextEmailAddress.text.toString(),
+                        editTextTextPassword2.text.toString()).addOnCompleteListener {
+                            if (it.isSuccessful){showHome(it.result?.user?.email ?: "", ProviderType.BASIC)}
+                            else{showAlert()}
+                    }
+            }else{
+                showAlert()
+            }
+        }
+    }
+
+    private fun showAlert(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se a producido un error autenticando al usuario")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+    private fun showHome(email: String, provider: ProviderType){
+        val intent = Intent(this, HomeActivity::class.java).apply {
+            putExtra("email", email)
+            putExtra("provider", provider.name)
+        }
+        startActivity(intent)
     }
 
 }
